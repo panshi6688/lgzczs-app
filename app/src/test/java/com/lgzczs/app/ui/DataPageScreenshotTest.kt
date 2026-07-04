7ceeb68eae18b44f1843e76c715fa5b4f3c4e163
panshi6688 <1120760396@qq.com>
2026-07-04 18:51:41 +0800

    fix: add mesa packages, use GraphicsMode.NATIVE

--- app/src/test/java/com/lgzczs/app/ui/DataPageScreenshotTest.kt
+++ app/src/test/java/com/lgzczs/app/ui/DataPageScreenshotTest.kt
@@ -15,7 +15,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
-@GraphicsMode(GraphicsMode.Mode.SOFTWARE)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
 class DataPageScreenshotTest {
 
     @get:Rule
